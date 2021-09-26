ff841965855151621744f422de24776f01c3f029
Sebby80 <sebkm80@gmail.com>
2021-09-26 17:53:55 +0530

    Adding genre buttons

--- components/NavBar.js
+++ components/NavBar.js
@@ -6,9 +6,11 @@ import logo from "../public/disney.png";
 const NavBar = ({ account }) => {
   return (
     <div className="navbar">
-      <Link href="/">
-        <Image src={logo} alt="Disney Logo" height={50} width={90} />
-      </Link>
+      <div className="logo-container">
+        <Link href="/">
+          <Image src={logo} alt="Disney Logo" height={50} width={90} />
+        </Link>
+      </div>
       <div className="account-info">
         <p>Welcome {account.username}</p>
         <img className="avatar" src={account.avatar.url} />
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,12 @@
 import { gql, GraphQLClient } from "graphql-request";
 import Section from "../components/Section";
 import NavBar from "../components/NavBar";
+import Link from "next/link";
+import Image from "next/image";
+import disney from "../public/disney-button.png";
+import marvel from "../public/marvel-button.png";
+import natgeo from "../public/natgeo-button.png";
+import starwars from "../public/star-wars-button.png";
 
 export const getStaticProps = async () => {
   const graphQLClient = new GraphQLClient(process.env.ENDPOINT, {
@@ -82,32 +88,56 @@ const Home = ({ videos, user }) => {
           <img src={randomVideo(videos).thumbnail.url} />
         </div>
         <div className="video-feed">
-          <Section
-            genre={"Recommended for you"}
-            videos={unseenVideos(videos)}
-          />
-          <Section genre={"Comedy"} videos={filterVideos(videos, "comedy")} />
-          <Section genre={"Sci-fi"} videos={filterVideos(videos, "sci-fi")} />
-          <Section
-            genre={"Thriller"}
-            videos={filterVideos(videos, "thriller")}
-          />
-          <Section genre={"Action"} videos={filterVideos(videos, "action")} />
-          <Section
-            genre={"Animation"}
-            videos={filterVideos(videos, "animation")}
-          />
-          <Section genre={"Marvel"} videos={filterVideos(videos, "marvel")} />
-          <Section
-            genre={"National Geographic"}
-            videos={filterVideos(videos, "travel")}
-          />
-          <Section genre={"Disney"} videos={filterVideos(videos, "disney")} />
-          <Section
-            genre={"Star Wars"}
-            videos={filterVideos(videos, "star wars")}
-          />
+          <Link href="#disney">
+            <div className="franchise" id="disney">
+              <Image src={disney} />
+            </div>
+          </Link>
+          <Link href="#marvel">
+            <div className="franchise" id="marvel">
+              <Image src={marvel} />
+            </div>
+          </Link>
+          <Link href="#natgeo">
+            <div className="franchise" id="natgeo">
+              <Image src={natgeo} />
+            </div>
+          </Link>
+          <Link href="#starwars">
+            <div className="franchise" id="starwars">
+              <Image src={starwars} />
+            </div>
+          </Link>
         </div>
+        <Section genre={"Recommended for you"} videos={unseenVideos(videos)} />
+        <Section genre={"Comedy"} videos={filterVideos(videos, "comedy")} />
+        <Section genre={"Sci-fi"} videos={filterVideos(videos, "sci-fi")} />
+        <Section genre={"Thriller"} videos={filterVideos(videos, "thriller")} />
+        <Section genre={"Action"} videos={filterVideos(videos, "action")} />
+        <Section
+          genre={"Animation"}
+          videos={filterVideos(videos, "animation")}
+        />
+        <Section
+          id="marvel"
+          genre={"Marvel"}
+          videos={filterVideos(videos, "marvel")}
+        />
+        <Section
+          id="natgeo"
+          genre={"National Geographic"}
+          videos={filterVideos(videos, "travel")}
+        />
+        <Section
+          id="disney"
+          genre={"Disney"}
+          videos={filterVideos(videos, "disney")}
+        />
+        <Section
+          id="starwars"
+          genre={"Star Wars"}
+          videos={filterVideos(videos, "starwars")}
+        />
       </div>
     </>
   );
